bc13d54466cdcc5c05fba1dd4560ed3d3ce19488
vniba <abinv321@gmail.com>
2022-10-31 19:01:15 +0530

    pract: arithmetic op | comparison | assignment | data types | variables | values

--- js-avodha/js/app.js
+++ js-avodha/js/app.js
@@ -1,1 +1,35 @@
-document.write(2 + 2 / 2);
+'use strict';
+// document.write(2 + 2 / 2); // ! don't use
+// window.alert('what !');
+console.warn(`i'am console 🤟`);
+
+// * values
+// fixed, literls
+// variable
+
+// literls -> it's fixed values
+//string '', ""
+// document.querySelector('#demoOne').innerHTML = 'hai ';
+// numbers   integer
+// document.querySelector('#demoOne').innerHTML = '1111';
+// floating point
+// document.querySelector('#demoOne').innerHTML = '2134.3';
+
+//  variabls
+// var, let, const
+// rules:  a-z, A-Z, _, $ & 0-9, cannot use special keywords
+
+/* const pi = 3.141592; // cannot reasign
+let firstName = 'jon'; // can reasign , block scope
+var age = 38; // can reasign , function scope /⚠️don't use
+
+// to select a same class in all element
+const box = document.querySelectorAll('.params');
+box.forEach((element) => {
+	element.innerHTML = 'pi';
+	element.style.fontSize = '2rem';
+});
+	*/
+
+// Case Sensitive age & Age are different
+// camelCase 🐫 -> firstName, lastName
