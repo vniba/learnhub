94a6364f17206d8cd42a0a886e68ceffc676927d
vniba <itsmescorona@gmail.com>
2022-12-02 09:50:38 +0530

    feat: Number
    number methods valueOf,  parseInt, parseFloat, max-value, min-value etc.

--- js-avodha/apps/numbers.js
+++ js-avodha/apps/numbers.js
@@ -65,7 +65,6 @@ const n = 10;
 console.log(n.toString(2)); //binary
 console.log(n.toString(8)); // octa
 console.log(n.toString(16)); // hexadecimal
-console.log(n.toString(32)); //
 
 // new keyword with Number
 let o = new Number(222);
@@ -78,3 +77,89 @@ console.log(q === p); // false
 const r = new Number(10);
 const s = new Number(10);
 console.log(r === s); //false
+
+// numbers method
+// toString(radix) - return value as string
+const nums = 10;
+console.log(nums.toString());
+
+// literals
+console.log((15).toString());
+
+// expression
+console.log((100 + 20).toString());
+
+// toExponential() - return value with exponetial notation
+const expe = 12.234;
+console.log(expe.toExponential(2));
+
+// toFixed - return value with fixed notion
+const fix = 3.1415;
+const fixd = 2.81;
+console.log(fix.toFixed(3));
+console.log(fixd.toFixed()); // 3
+
+//  toPrecision()
+const prec = 2.7182;
+console.log(prec.toPrecision(3));
+
+// toLcaleString() - return local string
+const local = 22e5;
+console.log(local.toLocaleString());
+
+// return value in primitive
+const isNum = 2e2;
+console.log(typeof isNum);
+console.log(isNum.valueOf());
+console.log((23 - 10).valueOf());
+
+// convert  to number - parseInt(), Number(), parsefloat()
+
+const numMeth = ' 10  0';
+const numMethFlo = '10.13';
+const numFal = true;
+
+console.log(parseInt(numMeth)); // convert string to number
+console.log(parseFloat(numMethFlo)); // floating to number
+
+console.log(Number(numFal)); // convert to num
+
+// number method used in date
+const dte = Number(new Date('1970-01-02')); // value in millisecond
+const starDate = Number(new Date('1970-01-01')); // value in millisecond b/w staring date to current date
+console.log(starDate);
+
+console.log(dte, typeof dte); // return full date
+
+// parseInt()
+const parse = -15.888;
+const parseStr = '12';
+console.log(parse);
+
+console.log(parseInt(parseStr)); // num need to first
+console.log(parseInt(parse));
+
+// parseFloat()
+const parseflot = '12.11';
+console.log(parseFloat(parseflot));
+
+// number properties
+
+// max value - return largest possible num in js
+// Min value - return smallest posiible num in js
+// infinity  - num after browser cannot handle
+// -infinity - negative ''
+//  NaN      - Not a Number
+// First-date = 1970-01-01
+
+const max = Number.MAX_VALUE;
+const min = Number.MIN_VALUE;
+const pInf = Number.POSITIVE_INFINITY;
+const nInf = Number.NEGATIVE_INFINITY;
+const naan = Number.NaN;
+
+console.log('max-value', max);
+console.log('min-value', min);
+console.log('infinity', pInf);
+console.log('-infinity', nInf);
+console.log('NaN', naan);
