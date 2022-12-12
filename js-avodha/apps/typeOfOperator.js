160e46cb86b11720c5437177be04b94a0b948e5c
vniba <itsmescorona@gmail.com>
2022-12-12 13:25:50 +0530

    feat: typeof
    typeof operator

---
+++ js-avodha/apps/typeOfOperator.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const num = 123
+const str = 'Elroy'
+const isLive = true
+const hai = null
+const defined = undefined
+
+console.log(typeof num, num);
+console.log(typeof str, str);
+console.log(typeof hai, hai);
+
+// objects
+// object, Array, Date, String, Number, Boolean
+
+const there = document.querySelector('#here')
+
+
