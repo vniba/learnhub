843c79c35e3dca5bb27b22e08f716012e8a753ba
vniba <itsmescorona@gmail.com>
2022-12-14 10:29:20 +0530

    feat: type conversion
    unary = -, + to convert str to num

--- js-avodha/apps/typeOfOperator.js
+++ js-avodha/apps/typeOfOperator.js
@@ -15,4 +15,36 @@ console.log(typeof hai, hai);
 
 const there = document.querySelector('#here')
 
+let user = {
+  fName: 'hitler'
+  , lName: 'Beer'
+  , age: 40
+}
+console.log(user, typeof user);
 
+user = null
+console.log(user);
+
+
+// ++++++++++++ type conversion ------------
+
+// number to string
+let stringy = '123'
+console.log(stringy, typeof stringy);
+
+let numStr = parseInt(stringy)
+console.log(numStr, typeof numStr);
+
+console.log(parseFloat('1.20'));
+
+let unary = + '2e5' //-- convert string to num
+console.log(unary)
+console.clear();
+
+// string to number
+let numbers = 987
+console.log(numbers.toString());
+
+let expo = 1509.12
+// console.log(expo.toExponential());
+console.log(expo.toPrecision(2))
