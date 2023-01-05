eafaccb53c44a1fc1e7ac9c20ad02c2c9ee1c022
vniba <itsmescorona@gmail.com>
2023-01-05 13:26:56 +0530

    feat: arguments
    length | default value |

--- js-avodha/core/function.js
+++ js-avodha/core/function.js
@@ -51,7 +51,6 @@ console.log(toStr);
 const water = () => `h2o`;
 console.log(water());
 
-console.clear();
 // no this
 const base = {
   proton: 1,
@@ -62,3 +61,39 @@ const base = {
   }
 };
 console.log(base.atom());
+
+
+// no typechecks in args
+function quark(up, down) {
+  return up * down;
+}
+console.log(quark('s', 1));
+
+// setting default values
+function total(a, b = 0) {
+  return a + b;
+}
+console.log(total(10));
+
+console.clear();
+
+function maxe() {
+  let max = -Infinity;
+  for (let i = 0; i < arguments.length; i++) {
+    if (arguments[i] > max) {
+      max = arguments[i];
+    }
+  }
+  return max;
+}
+
+console.log(maxe(6, 1, 7, 3, 6, 1.8, 10e1));
+
+function sumAll() {
+  let sum = 0;
+  for (let index = 0; index < arguments.length; index++) {
+    sum += arguments[index];
+  }
+  return sum;
+}
+console.log(sumAll(90, 70, 10));
