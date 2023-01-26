4cbcac1fc4bbd69374d8705a98c38bd15f08f129
vniba <itsmescorona@gmail.com>
2023-01-26 13:16:49 +0530

    fix: 3
    game events

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -503,3 +503,37 @@ console.log([...question]);
 console.log(question.entries());
 console.log(...question.keys());
 console.log(...question.values());
+
+// coding challenge no #3 🔴
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+console.clear();
+
+// ----------- 1 ------------
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// ---------- 2 -------------
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// ---------- 3 ------------
+const avgTime = 90 / gameEvents.size;
+console.log(`An event Happened, on average, every ${avgTime} minutes`);
+
+// ---------- 4 ------------
+for (const [key, value] of gameEvents) {
+  let str = key <= 45 ? 'First' : 'Second';
+  console.log(`[${str} Half] ${key} : ${value}`);
+}
