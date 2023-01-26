91dbbe966daabf16ca498578f8e18345853e758c
vniba <itsmescorona@gmail.com>
2023-01-26 11:55:55 +0530

    feat(1.1.1): DOM
    form validation

--- js-avodha/apps/dom.js
+++ js-avodha/apps/dom.js
@@ -12,29 +12,43 @@ btn[0].innerHTML = 'changed';
 
 document.querySelector('button').style.backgroundColor = 'blue';
 
-const butt = document.querySelector('#btnz');
-butt.addEventListener('click', () => {
-  let body = document.querySelector('body');
-  body.style.backgroundColor = 'gold';
+// const butt = document.querySelector('#btnz');
+// butt.addEventListener('click', () => {
+//   let body = document.querySelector('body');
+//   body.style.backgroundColor = 'gold';
 
-  // const box = document.querySelector('#box');
-  // box.classList.add(''hello'');
-});
+//   // const box = document.querySelector('#box');
+//   // box.classList.add(''hello'');
+// });
 
 // to find element  -> id, tag, class
 // by css selector
 
 let boxed = document.querySelectorAll('#box h1');
 
-boxed[0].style.color = 'yellow';
+// boxed[0].style.color = 'yellow';
 
 // to change element
 // --> innerHTML , set , innerText, style
 
-document.querySelector('#img').src = 'img/a_gangster_anime_by_darkbeam0_dfl5qdd-fullview.jpg';
+// document.querySelector('#img').src = 'img/a_gangster_anime_by_darkbeam0_dfl5qdd-fullview.jpg';
 
-document.querySelector('#param').innerHTML = Date();
+// document.querySelector('#param').innerHTML = Date();
 
 // for creating element
 const sub = document.createElement('h3');
 
+document.body.style.backgroundColor = 'green';
+console.log(document.body);
+console.clear();
+
+// form function
+function validateForm() {
+  const form = document.forms[1]['fname'].value;
+  if (form === '') {
+    alert('name is empty');
+    return false;
+  }
+  console.log(form);
+
+}
