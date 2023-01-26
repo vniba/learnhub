9cb6b947dbeb161d506dedde8950f8f4d25db5fb
vniba <itsmescorona@gmail.com>
2023-01-26 10:54:16 +0530

    feat(1.1.1): DOM
    querySelector | getElementsByTagName | classList | add | style | color

---
+++ js-avodha/apps/dom.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const demo = document.querySelector('#demoOne');
+demo.innerHTML = 'DOM 📄';
+
+demo.style.color = '#0ff';
+demo.style.fontSize = '3rem';
+
+const btn = document.getElementsByTagName('button');
+
+btn[0].innerHTML = 'changed';
+
+document.querySelector('button').style.backgroundColor = 'blue';
+
+const butt = document.querySelector('#btnz');
+butt.addEventListener('click', () => {
+  let body = document.querySelector('body');
+  body.style.backgroundColor = 'gold';
+
+  // const box = document.querySelector('#box');
+  // box.classList.add(''hello'');
+});
+
+// to find element  -> id, tag, class
+// by css selector
+
+let boxed = document.querySelectorAll('#box h1');
+
+boxed[0].style.color = 'yellow';
+
+// to change element
+// --> innerHTML , set , innerText, style
+
+document.querySelector('#img').src = 'img/a_gangster_anime_by_darkbeam0_dfl5qdd-fullview.jpg';
+
+document.querySelector('#param').innerHTML = Date();
+
+// for creating element
+const sub = document.createElement('h3');
+
