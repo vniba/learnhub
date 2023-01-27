a5253234aea18a9618f99484752b753721f4df89
vniba <itsmescorona@gmail.com>
2023-01-27 13:29:55 +0530

    feat: function
    this | call | bind | apply | parial fn | return a fn

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -68,4 +68,93 @@ function High5() {
 document.body.addEventListener('click', High5);
 
 ['doc', 'sis', 'ceo', 'bus'].forEach(High5);
+
+// return a fn
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const hey = greet('hey');
+hey('jon');
+
+greet('daa')('pulle');
+
+// Challenge
+const greets = greeting => name => console.log(`${greeting} ${name}`);
+
+greets('dee')('vde');
+
+const lufthansa = {
+  airLine: 'lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNo, name) {
+    console.log(
+      `${name} booked a seat on ${this.airLine} flight ${this.iataCode}${flightNo}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNo}`, name });
+  },
+};
+
+lufthansa.book(201, 'max swell');
+console.log(lufthansa.bookings);
+
+const escrowing = {
+  airLine: 'escrowing',
+  iataCode: 'EC',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// call method
+book.call(escrowing, 201, 'kim dae');
+console.log(escrowing);
+
+book.call(lufthansa, 101, 'mia markova');
+
+// apply method
+const flightDate = [410, 'jesus crack'];
+book.apply(escrowing, flightDate);
+
+// use this 🟢🟢🟢🟢🟢🟢🟢
+book.call(escrowing, ...flightDate);
+
+const bookLu = book.bind(lufthansa);
+bookLu(93, 'lol fea');
+
+const bookEc = book.bind(escrowing);
+bookEc(49, 'angeline lo');
+
+const bookEc34 = book.bind(escrowing, 34);
+bookEc34('mariana sun');
+
 console.clear();
+
+// With event listeners
+lufthansa.planes = 148;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial Application
+const addTax = (rate, value) => value + value * rate;
+
+console.log(addTax(0.2, 100));
+
+const addVAT = addTax.bind(null, 0.23);
+console.log(addVAT(100));
+
+// Challenge
+const addTAX = value => rate => value + value * rate;
+
+console.log(addTAX(100)(0.5));
