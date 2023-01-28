24c7788ad1b0666860cc9b750452d6823d687613
vniba <itsmescorona@gmail.com>
2023-01-28 11:38:35 +0530

    perf: array
    slice | splice | reverse | concat | at | for of | forEach

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -71,6 +71,57 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+let arr = ['z', 'y', 'x', 'w'];
+// slice
+console.log(arr.slice(0, 2));
+console.log(arr.slice(-2));
+console.log(arr.slice());
+
+// Splice
+console.log(arr.splice(-1));
+console.log(arr);
+
+// reverse
+arr = ['z', 'y', 'x', 'w'];
+
+const arr1 = ['h', 'i', 'g', 'k'];
+console.log(arr1.reverse());
+console.log(arr1);
+
+// Concat
+const letters = arr.concat(arr1);
+console.log(letters);
+console.log([...arr, ...arr1]);
+
+// join
+console.log(letters.join('.'));
+
+// at()
+const array = [10, 20, 30, 40];
+console.log(array.at(0));
+
+// last element
+console.log(array.at(-1));
+
+console.log('hello'.at(0));
+
+// loop
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const [i, move] of movements.entries()) {
+  if (move > 0) {
+    console.log(`Movement ${i + 1} you deposited ${move}💲`);
+  } else console.log(`you Withdraw ${Math.abs(move)}💲`);
+}
+console.clear();
+
+movements.forEach((a, i, array) => {
+  a > 0
+    ? console.log(`${i + 1} you deposited ${a}💲`)
+    : console.log(`${i + 1} you Withdraw ${Math.abs(a)}💲`);
+  // console.log(array);
+});
+// break and continue not work 👆
