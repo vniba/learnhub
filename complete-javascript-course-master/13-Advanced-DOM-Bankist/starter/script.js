b7b161709b2631350594f891653523ba7d8b6c90
vniba <itsmescorona@gmail.com>
2023-02-06 12:12:31 +0530

    feat: DOM
    traversing | childnode,children|parent | previousElementSibling, next|

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -133,3 +133,33 @@ document.querySelector('.nav').addEventListener(
   },
   true
 );
+
+// DOM Traversing
+
+// downwards
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+
+h1.firstElementChild.style.color = 'red';
+h1.lastElementChild.style.color = 'yellow';
+
+// going upwards
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary';
+
+// sideways -> siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(e => {
+  if (e !== h1) {
+    e.style.transform = 'scale(0.5)';
+  }
+});
