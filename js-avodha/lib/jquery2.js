ea7b130d8fdaf2ce88ec685be971c83e67624ad9
vniba <itsmescorona@gmail.com>
2023-02-06 15:37:45 +0530

    feat: jquery
    fadeIn | fadeOut | fadeToggle | fadeTo

--- js-avodha/lib/jquery2.js
+++ js-avodha/lib/jquery2.js
@@ -8,3 +8,45 @@ $('.btn-s').click(() => $('#h').show(2000));
 //toggle
 $('#all').click(() => $('#h').toggle(1000));
 
+// fade
+// fadeIn/ fadeOut/ fadeToggle/ fadeTo
+
+$(document).ready(function () {
+
+  // fadeIn
+  $('button').click(() => {
+    // $('#div1').fadeIn(2000);
+
+    // $('#div2').fadeIn();
+
+    // $('#div3').fadeIn('medium');
+
+    // fadeOut
+    // $('#div1').fadeOut(2000);
+
+    // $('#div2').fadeOut();
+
+    // $('#div3').fadeOut('medium');
+
+    // fadeToggle
+    // $('#div1').fadeToggle(2000);
+
+    // $('#div2').fadeToggle();
+
+    // $('#div3').fadeToggle('medium');
+
+    // fadeTo
+    // $('#div1').fadeTo(2000, 0.5);
+
+    // $('#div2').fadeTo(5000, 0.9);
+
+    // $('#div3').fadeTo('medium', 0.2);
+
+    // ----------
+    $('#div1').fadeOut(2000);
+
+    $('#div2').fadeToggle(5000);
+
+    $('#div3').fadeTo('medium', 0.2);
+  });
+});
