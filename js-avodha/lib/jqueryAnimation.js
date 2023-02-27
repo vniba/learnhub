342e8cc8ee1136004b9f07f1602cfafbc060126e
vniba <itsmescorona@gmail.com>
2023-02-27 19:13:38 +0530

    feat: jquery
    remove | add | toggle | css | addClass

--- js-avodha/lib/jqueryAnimation.js
+++ js-avodha/lib/jqueryAnimation.js
@@ -4,12 +4,10 @@
 // animate({param},speed,callback)
 
 $(document).ready(function () {
-
   // $('#btn').click(() => $('.div').animate({ right: 0 }, 2000));
-
   // multiple properties
-  $('#btn').dblclick(() => {
-    $('.div').animate({ right: 0, borderRadius: '50%' }, 1500);
+  /*  $("#btn").dblclick(() => {
+    $(".div").animate({ right: 0, borderRadius: "50%" }, 1500);
   });
 
   // properties should be camelCase 🐪
@@ -28,16 +26,15 @@ $(document).ready(function () {
 
   // predefined values
 
-  $('#btn').click(() => {
-
-    $('.box').animate({
-      height: 'toggle',
+  $("#btn").click(() => {
+    $(".box").animate({
+      height: "toggle",
     });
   });
 
   // queue functionality
-  $('#btnPlus').click(() => {
-    let div = $('.box');
+  $("#btnPlus").click(() => {
+    let div = $(".box");
     // div.animate({
     //   height: '100px',
     // }, 1000);
@@ -54,7 +51,56 @@ $(document).ready(function () {
     //   height: '50px'
     // }, 1000);
 
-    div.animate({ left: '100px' });
+    div.animate({ left: "100px" });
+  }); */
+  /*   $('#btn').click(() => {
+    $('p').html(
+      '<h3>Blanditiis</h3> est quia. Velit assumenda aut ut dolorem odit omnis dicta. A dignissimos aut accusamus accusantium eius. Asperiores veritatis molestiae.'
+    ); */
+  /*  $('#btn').click(() => {
+    $('#name').val('modified');
+  }); */
+
+  // callback
+  /*  $('#btn').click(() => {
+    // $('#param').text((i, oText) => {
+    //   return oText + i;
+    // });
+    $('a').attr('href', (i, val) => {
+      return (val = 'http://localhost:5050');
+    });
+  }); */
+
+  const pr = 'prepend ';
+
+  $('#btn').click(() => {
+    // $('h2').append(' <p>append method</p>');
+    // $('h2').prepend(pr);
+    // $('h2').after(`<p>after</p>`);
+    // $('h2').before(`<p>before</p>`);
   });
 
+  // removing element
+  $('#btn').click(() => {
+    // $('div').remove();
+    // $('div').empty();
+    // $('p').remove('.first');
+  });
+
+  // Adding Classes
+  // $('#btn').click(() => {
+  //   $('p').addClass('dark ');
+  //   $('.first').addClass('danger light');
+  // });
+  // // Remove, toggle
+  // $('#btn2').click(() => {
+  //   $('.first').removeClass('dark');
+  //   $('.first').toggleClass('danger');
+  // });
+
+  // css
+  $('#btn').click(() => {
+    // $('.first').css('backgroundColor', 'greenyellow');
+    $('.first').css({ color: 'aqua', backgroundColor: 'gray' });
+  });
 });
