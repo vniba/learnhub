f3897566ba5c2b7b2ca90d503c4ef682cb0f88cc
vn!ba <itsmescorona@gmail.com>
2023-05-29 10:07:08 +0530

    sort game

--- game-dex/src/App.tsx
+++ game-dex/src/App.tsx
@@ -11,6 +11,7 @@ import SortSelector from "./components/SortSelector.tsx";
 export interface GameQuery {
   genre: Genres | null;
   platform: Platform | null;
+  sortOrder: string;
 }
 function App() {
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
@@ -22,28 +23,32 @@ function App() {
           base: `"nav" "main"`,
           lg: `"nav nav" "aside main"`,
         }}
-        templateColumns={{ base: "1fr", lg: "200px 1fr" }}
-      >
-        <GridItem area="nav">
+        templateColumns={{ base: "1fr", lg: "200px 1fr" }}>
+        <GridItem area='nav'>
           <NavBar />
         </GridItem>
-        <Show above="lg">
-          <GridItem area="aside" paddingX={2}>
+        <Show above='lg'>
+          <GridItem area='aside' paddingX={2}>
             <GenreList
-              onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+              onSelectGenre={genre => setGameQuery({ ...gameQuery, genre })}
               selectedGenre={gameQuery.genre}
             />
           </GridItem>
         </Show>
-        <GridItem area="main">
+        <GridItem area='main'>
           <HStack paddingLeft={12} spacing={5}>
             <PlatformSelector
               selectedPlatform={gameQuery.platform}
-              onSelectPlatform={(platform) =>
+              onSelectPlatform={platform =>
                 setGameQuery({ ...gameQuery, platform })
               }
             />
-            <SortSelector />
+            <SortSelector
+              onSelectSortOrder={sortOrder =>
+                setGameQuery({ ...gameQuery, sortOrder })
+              }
+              sortOrder={gameQuery.sortOrder}
+            />
           </HStack>
           <GameGrid gameQuery={gameQuery} />
         </GridItem>
--- game-dex/src/components/SortSelector.tsx
+++ game-dex/src/components/SortSelector.tsx
@@ -1,15 +1,55 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/all";
 
-const SortSelector = () => {
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  sortOrder: string;
+}
+
+const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
+  const sortOrders = [
+    {
+      value: "",
+      label: "Relevance",
+    },
+    {
+      value: "-added",
+      label: "Date added",
+    },
+    {
+      value: "name",
+      label: "Name",
+    },
+    {
+      value: "-released",
+      label: "Release date ",
+    },
+    {
+      value: "-metacritic",
+      label: "Popularity",
+    },
+    {
+      value: "-rating",
+      label: "Average ratings",
+    },
+  ];
+
+  const currentSortOrder = sortOrders.find(order => order.value === sortOrder);
+
   return (
     <>
       <Menu>
-        <MenuButton fontWeight="bold" as={Button} rightIcon={<BsChevronDown />}>
-          Sort by:nothing
+        <MenuButton fontWeight='bold' as={Button} rightIcon={<BsChevronDown />}>
+          Sort by: {currentSortOrder?.label || "Relevance"}
         </MenuButton>
         <MenuList>
-          <MenuItem>relevance</MenuItem>
+          {sortOrders.map(order => (
+            <MenuItem
+              onClick={() => onSelectSortOrder(order.value)}
+              key={order.value}>
+              {order.label}
+            </MenuItem>
+          ))}
         </MenuList>
       </Menu>
     </>
