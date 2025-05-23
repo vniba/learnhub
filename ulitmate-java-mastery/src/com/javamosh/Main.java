8c496148f28eb85ed7de0f0dbd19b91af0e3ec96
vniba <vnibax@gmail.com>
2025-05-23 16:00:23 +0530

    Add reference and string methods demonstrating Java object references and string manipulation; comment out unused print statement

--- ulitmate-java-mastery/src/com/javamosh/Main.java
+++ ulitmate-java-mastery/src/com/javamosh/Main.java
@@ -1,5 +1,8 @@
 package com.javamosh;
 
+import java.awt.*;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         byte age = 99;
@@ -7,6 +10,26 @@ public class Main {
         long viewsCount = 2000_0000_000L;
         double price = 10.20;
         char letter = 'A';
-        System.out.printf("%,d %f %c", viewsCount, price,letter);
+//        System.out.printf("%,d %f %c", viewsCount, price,letter);
+//        Main.reference();
+        Main.string();
+    }
+
+    public static void reference() {
+        byte age = 12;
+        Date date = new Date();
+        byte x = 1;
+        byte y = x;
+        x = 2;
+        Point point = new Point(x, 2);
+        Point point2 = point;
+        point.x = 3;
+        System.out.println(point2.getX());
+    }
+
+    public static void string() {
+        String msg = "Hello World! \uD83D\uDE06";
+        String ne = msg.replace('H', 'N');
+        System.out.printf("%s -> %d %s", msg, msg.indexOf('l'), ne);
     }
 }
