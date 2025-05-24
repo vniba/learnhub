2e4a4c62bada3a7df675fdb3fe14cad9143063f2
vniba <vnibax@gmail.com>
2025-05-24 12:58:20 +0530

    Refactors mortgage calculation with input validation.

--- java/ulitmate-java-mastery/src/com/javamosh/Mortgage.java
+++ java/ulitmate-java-mastery/src/com/javamosh/Mortgage.java
@@ -6,21 +6,69 @@ import java.util.Scanner;
 
 class Mortgage {
     public static void main(String[] args) {
+        mortgage();
+    }
+
+    public static void mortgage() {
+
+
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal : ");
-        long principal = scanner.nextLong();
-
-        System.out.print("Annual Interset Rate : ");
-        float rate = scanner.nextFloat();
-
-        System.out.print("Period (Years) : ");
-        int year = scanner.nextInt();
+        double mortgage = 0.0;
+        long principal = getPrincipal(scanner);
+        float annualInterest = getAnnualInterest(scanner);
+        int year = getYear(scanner);
 
 
-        double mortgage = Mortgage.calculateMortgage(principal, rate, year);
-
+        mortgage = Mortgage.calculateMortgage(principal, annualInterest, year);
         System.out.printf("Mortgage: %s", NumberFormat.getCurrencyInstance().format(mortgage));
+
         scanner.close();
+    }
+
+    private static byte getYear(Scanner scanner) {
+        final byte MIN_YEAR = 1;
+        final byte MAX_YEAR = 30;
+        byte year = 0;
+        while (true) {
+            System.out.print("Period (Years) : ");
+            year = scanner.nextByte();
+            if (year < MIN_YEAR || year > MAX_YEAR) {
+                System.out.printf("%s %s and %s", "Enter a Value b/w", MIN_YEAR, MAX_YEAR).println();
+            } else break;
+        }
+        return year;
+    }
+
+    private static float getAnnualInterest(Scanner scanner) {
+        final byte MIN_ANNUAL_INTEREST = 1;
+        final byte MAX_ANNUAL_INTEREST = 30;
+
+        float annualInterest = 0;
+        while (true) {
+            System.out.print("Annual Interset Rate : ");
+            annualInterest = scanner.nextFloat();
+            if (annualInterest < MIN_ANNUAL_INTEREST || annualInterest > MAX_ANNUAL_INTEREST) {
+                System.out.printf("%s %s and %s", "Enter a Value b/w", MIN_ANNUAL_INTEREST, MAX_ANNUAL_INTEREST).println();
+            } else break;
+        }
+        return annualInterest;
+    }
+
+    private static long getPrincipal(Scanner scanner) {
+        final int MIN_PRINCIPAL = 1000;
+        final int MAX_PRINCIPAL = 1000_000;
+        long principal = 0;
+        while (true) {
+            System.out.print("Principal : ");
+            principal = scanner.nextLong();
+            if (principal < MIN_PRINCIPAL || principal > MAX_PRINCIPAL) {
+                System.out.printf("%s %s and %s", "Enter a Num b/w", NumberFormat.getCurrencyInstance().format(MIN_PRINCIPAL),
+                        NumberFormat.getCurrencyInstance().format(MAX_PRINCIPAL)).println();
+            } else break;
+
+
+        }
+        return principal;
     }
 
     private static double calculateMortgage(double principal, double annualInterestRate, int totalYear) {
