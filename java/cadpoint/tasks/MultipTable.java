02853f35fb067682f9afc6d5ae10ef8620066ac3
vniba <vnibax@gmail.com>
2025-05-30 10:05:33 +0530

    cadpoint: Multiplication table number task

--- java/cadpoint/tasks/MultipTable.java
+++ java/cadpoint/tasks/MultipTable.java
@@ -1,22 +1,35 @@
-public class MultipTable{
+package tasks;
+public class MultipTable {
 
-	public static void main(String[] args){
+    public static void main(String[] args) {
 
 
-		PrintMultTable(2,5);		
-	}
+        PrintMultTable(2, 5);
+        printMultiplicationTableDo(2, 5);
+    }
 
-	private static void PrintMultTable(int num,int limit){
+    private static void printMultiplicationTableDo(int num, int limit) {
+        int start = 1;
 
-		int counter = 1;
+        do {
+            int sum = start * num;
+            System.out.printf("%d * %d = %d\n", start, num, sum);
+            start++;
+        } while (start <= limit);
+    }
 
-		while(counter<=limit){
-		
-			int sum = num*counter;
-			System.out.printf("%d * %d = %d\n",counter,num,sum);
-			
-			counter++;
-		}
-	}
-	
+
+    private static void PrintMultTable(int num, int limit) {
+
+        int counter = 1;
+
+        while (counter <= limit) {
+
+            int sum = num * counter;
+            System.out.printf("%d * %d = %d\n", counter, num, sum);
+
+            counter++;
+        }
+    }
+
 }
