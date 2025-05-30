1ba5755e8228c6edcc4a6e01b23baec9b4f33ad5
vniba <vnibax@gmail.com>
2025-05-30 10:04:57 +0530

    cadpoint: Armstrong number task

--- java/cadpoint/tasks/AmsOrPal.java
+++ java/cadpoint/tasks/AmsOrPal.java
@@ -1,43 +1,67 @@
-public class AmsOrPal{
+package tasks;
 
-	public static void main(String[] args){
-		int amsNum = 153;
-		boolean isArms = isArmstrongNum(amsNum);
+// 153 -> 1 cube + 5 cube + 3 cube -> 153
+public class AmsOrPal {
 
-	//	printOut(amsNum,isArms);		
-		
-		printArmstrongNumbers(100,1000);
-	}
+    public static void main(String[] args) {
+        int amsNum = 153;
+        boolean isArms = isArmstrongNumberDo(amsNum);
 
-	private static void printOut(int number,boolean isArms){
-	
-		String is = isArms ?"Amstrong Number":"Not Amstrong Number";		
-		System.out.printf("%d is %s\n",number,is);
-	}
+        printOut(amsNum, isArms);
 
-	private static void printArmstrongNumbers(int start,int end){
+//        printArmstrongNumbers(100, 1000);
+        printArmstrongNumbersDo(100, 1000);
+    }
 
-		int counter = start;
-		
-		while(counter < end){
-			boolean isArms = isArmstrongNum(counter);
-			if(isArms) printOut(counter,isArms);
-			
-			counter++;
-		}
-	}
-	
-	private static boolean isArmstrongNum(int num){
-		
-		int total = 0;
-		int ams = num;
-		while(ams>0){
-			int digit = ams % 10;
-			total += digit * digit * digit;
-			ams = ams/10;
-		}
-		return num == total;
-	}
+    private static boolean isArmstrongNumberDo(int number) {
+        int val = number;
+        int sum = 0;
+        do {
+            int rem = val % 10;
+            sum += rem * rem * rem;
+            val = val / 10;
+        } while (val > 0);
+        return number == sum;
+    }
+
+    private static void printArmstrongNumbersDo(int start, int end) {
+        do {
+            boolean isArmsNum = isArmstrongNumberDo(start);
+            if (isArmsNum) printOut(start, true);
+            start++;
+        }
+        while (start < end);
+    }
+
+    private static void printOut(int number, boolean isArms) {
+
+        String is = isArms ? "Amstrong Number" : "Not Amstrong Number";
+        System.out.printf("%d is %s\n", number, is);
+    }
+
+    private static void printArmstrongNumbers(int start, int end) {
+
+        int counter = start;
+
+        while (counter < end) {
+            boolean isArms = isArmstrongNum(counter);
+            if (isArms) printOut(counter, isArms);
+
+            counter++;
+        }
+    }
+
+    private static boolean isArmstrongNum(int num) {
+
+        int total = 0;
+        int ams = num;
+        while (ams > 0) {
+            int digit = ams % 10;
+            total += digit * digit * digit;
+            ams = ams / 10;
+        }
+        return num == total;
+    }
 
 
 }
