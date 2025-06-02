1495a841c5114fc3bde3bc30f5e7078ec750adfc
vniba <vnibax@gmail.com>
2025-06-02 10:26:27 +0530

    chore: licence change and add nested for

---
+++ java/cadpoint/basics/NestedFor.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class NestedFor {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.printf("i: %d | j: %d\n", i, j);
+            }
+        }
+    }
+}
